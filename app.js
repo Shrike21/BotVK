9382a3b1323fdbe6e5edc8d5030408dc9042e867
Shrike21 <Alesha.novel@yandex.ru>
2018-09-17 18:06:52 +0300

    123

--- app.js
+++ app.js
@@ -1,40 +1,34 @@
-const VkBot = require('node-vk-bot-api')
-const Scene = require('node-vk-bot-api/lib/scene')
-const Session = require('node-vk-bot-api/lib/session')
-const Stage = require('node-vk-bot-api/lib/stage')
-const TOKEN = "7a550c238fc37c8762238b63c3fef90b4d2463393290a9a196cd5115c37beef4a4fd78cf757ad65bf2738"
-const GROUP_ID = "170925362"
- 
-const bot = new VkBot({
-  token: process.env.TOKEN,
-  group_id: process.env.GROUP_ID,
+const express = require('express');
+const bodyParser = require('body-parser')
+const  PORT = 3000;
+const { Botact } = require('botact')
+const bot = new Botact({
+  confirmation: "31e7993a",
+  token: "7a550c238fc37c8762238b63c3fef90b4d2463393290a9a196cd5115c37beef4a4fd78cf757ad65bf2738"
 })
-const scene = new Scene('meet',
-  (ctx) => {
-    ctx.scene.next()
-    ctx.reply('How old are you?')
-  },
-  (ctx) => {
-    ctx.session.age = +ctx.message.text
- 
-    ctx.scene.next()
-    ctx.reply('What is your name?')
-  },
-  (ctx) => {
-    ctx.session.name = ctx.message.text
- 
-    ctx.scene.leave()
-    ctx.reply(`Nice to meet you, ${ctx.session.name} (${ctx.session.age} years old)`)
-  }
-)
-const session = new Session()
-const stage = new Stage(scene)
- 
-bot.use(session.middleware())
-bot.use(stage.middleware())
- 
-bot.command('/meet', (ctx) => {
-  ctx.scene.enter('meet')
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+/*app.post('/', (req, res) => {
+const {body} = req;
+switch(body.type) {
+	case 'confirmation':
+	res.end('31e7993a');
+	break;
+
+	case 'message_new':
+	res.end('ok');
+	break;
+	default:
+	res.end('31e7993a');
+	break;
+}
+});*/
+bot.command('start', ({ reply }) => {
+  reply('This is start!')
 })
- 
-bot.startPolling()
+app.post('/', bot.listen)
+app.listen(PORT, () => console.log('Example app listening on port ', PORT));
