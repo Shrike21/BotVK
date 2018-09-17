e7725d2c1c88239a4d44de528e18d08bca2de932
Shrike21 <Alesha.novel@yandex.ru>
2018-09-17 15:32:23 +0300

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const VkBot = require('node-vk-bot-api')
+const Scene = require('node-vk-bot-api/lib/scene')
+const Session = require('node-vk-bot-api/lib/session')
+const Stage = require('node-vk-bot-api/lib/stage')
+const TOKEN = "7a550c238fc37c8762238b63c3fef90b4d2463393290a9a196cd5115c37beef4a4fd78cf757ad65bf2738"
+const GROUP_ID = "170925362"
  
 const bot = new VkBot({
-  token: "7a550c238fc37c8762238b63c3fef90b4d2463393290a9a196cd5115c37beef4a4fd78cf757ad65bf2738",
-  group_id: "170925362"
+  token: process.env.TOKEN,
+  group_id: process.env.GROUP_ID,
 })
+const scene = new Scene('meet',
+  (ctx) => {
+    ctx.scene.next()
+    ctx.reply('How old are you?')
+  },
+  (ctx) => {
+    ctx.session.age = +ctx.message.text
  
-bot.command('/start', (ctx) => {
-  ctx.reply('Hello!')
+    ctx.scene.next()
+    ctx.reply('What is your name?')
+  },
+  (ctx) => {
+    ctx.session.name = ctx.message.text
+ 
+    ctx.scene.leave()
+    ctx.reply(`Nice to meet you, ${ctx.session.name} (${ctx.session.age} years old)`)
+  }
+)
+const session = new Session()
+const stage = new Stage(scene)
+ 
+bot.use(session.middleware())
+bot.use(stage.middleware())
+ 
+bot.command('/meet', (ctx) => {
+  ctx.scene.enter('meet')
 })
  
 bot.startPolling()
