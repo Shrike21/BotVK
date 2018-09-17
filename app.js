7c2b340b9cd215c16f27fd36e5e2f14a71f627ca
Shrike21 <Alesha.novel@yandex.ru>
2018-09-17 19:31:49 +0300

    1111

--- app.js
+++ app.js
@@ -8,10 +8,21 @@ const bot = new Botact({
 })
 
 const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
- app.get('/', (req, res) => res.send('Hello World!')); 
+/* app.get('/', (req, res) => res.send('Hello World!')); */
 /*app.post('/', (req, res) => {
 const {body} = req;
 switch(body.type) {
